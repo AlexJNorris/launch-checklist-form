e1caacc9f4b7467730443d054ea4d61e90f4d026
Alex Norris <a.norris97@icloud.com>
2020-09-22 23:06:37 -0500

    fix validation

--- script.js
+++ script.js
@@ -13,40 +13,41 @@
 */
 
 		
-               
-
-window.addEventListener("load", function() {
-         let form = document.querySelector("form");
-         form.addEventListener("submit", function() {
+function handleForm() {
 		let pname = document.querySelector("input[name=pilotName]");
                 let cname = document.querySelector("input[name=copilotName]");
                 let flevel = document.querySelector("input[name=fuelLevel]");
-                let cmass = document.querySelector("input[name=cargoMass]");		
+                let cmass = document.querySelector("input[name=cargoMass]");
+		
+		pTest = Number(pname.value);
+		coTest = Number(cname.value);
+
+                if (pname.value === "" || cname.value === "" || flevel.value === "" || cmass.value === "") {
+                        alert("All fields are required!");
+                }
 		
 		flevel.value = Number(flevel.value);
                 cmass.value = Number(cmass.value);
-
-		if (pname.value === "" || cname.value === "" || flevel.value === "" || cmass.value === "") {
-                        alert("All fields are required!");
-                } else if ((typeof(pname.value) !== 'string') || (typeof(cname.value) !== 'string')){
+ 
+		if ( !isNaN(pTest) || !isNaN(coTest)){
                         alert("Pilot and Copilot names must be strings!");
                 } else if (isNaN(flevel.value) || isNaN(cmass.value)){
                         alert("Fuel level  and cargo mass values  must be numbers!");
                 } else {
-				
-		let pstatus = document.getElementById("pilotStatus");
+
+                let pstatus = document.getElementById("pilotStatus");
                 let copstatus = document.getElementById("copilotStatus");
                 let fstatus = document.getElementById("fuelStatus");
                 let cstatus = document.getElementById("cargoStatus");
                 let fItems = document.getElementById("faultyItems");
                 let lStatus = document.getElementById("launchStatus");
-	
-		 pstatus.innerHTML += ": ";
-                 pstatus.innerHTML += pname;
+
+                 pstatus.innerHTML += ": ";
+                 pstatus.innerHTML += pname.value;
                  pstatus.innerHTML += " is ready for launch!";
 
                  copstatus.innerHTML += ": ";
-                 copstatus.innerHTML += cname;
+                 copstatus.innerHTML += cname.value;
                  copstatus.innerHTML += " is ready for launch!";
 
                  if (flevel.value < 10000){
@@ -54,8 +55,12 @@ window.addEventListener("load", function() {
                          lStatus.innerHTML = "Shuttle not ready for launch";
                          lStatus.style.color = "red";
                  }
-	
-		}
-		return false;
-      });
+
+                }
+           
+}               
+
+window.addEventListener("load", function() {
+         let form = document.querySelector("form");
+         form.addEventListener("submit", handleForm);
 });
